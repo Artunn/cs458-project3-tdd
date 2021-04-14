00a02c6cfd2be66234ba4b486dbaf305b63c4a97
Artun <curaartun@gmail.cm>
2021-04-14 19:54:32 +0300

    only test code part

--- webpage-source/tests/partone.test.js
+++ webpage-source/tests/partone.test.js
@@ -1,1 +1,9 @@
 const partone = require('../partone');
+
+test('Check coordinate text',()=>{
+  expect(partone.controlCoordinateString("+90.0, -127.554334")).toBe(true)
+  expect(partone.controlCoordinateString("8, 27")).toBe(true)
+  expect(partone.controlCoordinateString("47.1231231, -179.9999999")).toBe(true)
+  expect(partone.controlCoordinateString("Hello friend")).toBe(false)
+  expect(partone.controlCoordinateString("53?x/")).toBe(false)
+})
