0aacdbecb710c8958c6fab585924b6e2f429a508
Artun <curaartun@gmail.cm>
2021-04-15 00:03:48 +0300

    Added parttwo test and realcode

--- webpage-source/partone.js
+++ webpage-source/partone.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 function controlCoordinate(coordinateNum1, coordinateNum2) {
     if (!(isNaN(coordinateNum1) || isNaN(coordinateNum2))) {
         if ((coordinateNum1 && coordinateNum2 <= 180) && coordinateNum1 && coordinateNum2 >= -180) {
@@ -7,4 +9,18 @@ function controlCoordinate(coordinateNum1, coordinateNum2) {
     return false;
 }
 
-module.exports = {controlCoordinate}
+function getCity(coordinateNum1, coordinateNum2) {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinateNum1},${coordinateNum2}&key=AIzaSyDFr6F19kpa0aYoBWUM11xDPQQfMeS5vw0`;
+    return axios.get(url).then(function(response){
+        let data = response.data.results[0].address_components;
+        // console.log(data);
+        for(let i = 0; i < data.length; i++)
+        {
+            if(data[i].types[0] == "administrative_area_level_1"){
+                return data[i].long_name;
+            }
+        }
+    });
+}
+
+module.exports = { controlCoordinate, getCity }
--- webpage-source/tests/partone.test.js
+++ webpage-source/tests/partone.test.js
@@ -8,8 +8,9 @@ test('Check coordinate value',()=>{
   expect(partone.controlCoordinate(90,"X")).toBe(false);
 })
 
-test('Check get city function',()=>{
-  expect(partone.getCity(39.9334,32.8597)).toMatch('/[A|a]nkara');
-  expect(partone.getCity(41.0082, 28.9784)).toMatch('/[I|i]stanbul');
-  expect(partone.getCity(40.4168,-3.7038)).toMatch('/[M|m]adrid');
+test('Check get city function',async ()=>{
+  expect.assertions(3);
+  await partone.getCity(39.9334,32.8597).then(data=>{ expect(data).toMatch(/[A|a]nkara/)});
+  await partone.getCity(41.0082, 28.9784).then(data=>{ expect(data).toMatch(/[I|i|İ]stanbul/)});
+  await partone.getCity(40.4168,-3.7038).then(data=>{ expect(data).toMatch(/[M|m]adrid/)});
 })
