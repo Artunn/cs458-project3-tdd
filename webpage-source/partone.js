510a8bf7e5e4446d2c0c5339e82c0a16c302ff01
Artun <curaartun@gmail.cm>
2021-04-15 23:53:59 +0300

    parta test code is almost done

--- webpage-source/partone.js
+++ webpage-source/partone.js
@@ -1,27 +1,16 @@
-const axios = require('axios');
-
 function controlCoordinate(coordinateNum1, coordinateNum2) {
-    if (!(isNaN(coordinateNum1) || isNaN(coordinateNum2))) {
-        if ((coordinateNum1 && coordinateNum2 <= 180) && coordinateNum1 && coordinateNum2 >= -180) {
+    if(coordinateNum1 === "" || coordinateNum2 === "" ){
+        return false;
+    }
+    if (Number.isNaN(coordinateNum1) || Number.isNaN(coordinateNum2)) {
+       return false;
+    }else{
+        if ((coordinateNum1 < 180 && coordinateNum2 < 180) && (coordinateNum1 > -180 && coordinateNum2 > -180)) {
             return true;
         }
     }
     return false;
 }
-
-function getCity(coordinateNum1, coordinateNum2) {
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinateNum1},${coordinateNum2}&key=AIzaSyDFr6F19kpa0aYoBWUM11xDPQQfMeS5vw0`;
-    return axios.get(url).then(function(response){
-        let data = response.data.results[0].address_components;
-        for(let i = 0; i < data.length; i++)
-        {
-            if(data[i].types[0] == "administrative_area_level_1"){
-                return data[i].long_name;
-            }
-        }
-    });
-}
-
 
 let map;
 function initMap() {
@@ -31,4 +20,53 @@ function initMap() {
   });
 }
 
-module.exports = { controlCoordinate, getCity }
+function buttonTrigger(){
+    let coordinate1 = document.getElementById("parta-coordinate1").value;
+    let coordinate2 = document.getElementById("parta-coordinate2").value;
+    if(controlCoordinate(coordinate1, coordinate2)){
+        const pos = {
+            lat: parseFloat(coordinate1),
+            lng: parseFloat(coordinate2)
+          };
+        console.log(pos)
+        map.setCenter(pos);
+        getCity(coordinate1,coordinate2,document.getElementById("parta-textdata"));
+    }else{
+        getCity(coordinate1,coordinate2,document.getElementById("parta-textdata"));
+    }
+}
+
+function makeRequest (method, url, done) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+
+      done(JSON.parse(xhr.response));
+    };
+    xhr.onerror = function () {
+        console.log("bbb")
+      done(JSON.parse(xhr.response));
+    };
+    xhr.send();
+}
+
+function getCity(coordinateNum1, coordinateNum2,item) {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinateNum1},${coordinateNum2}&key=AIzaSyDFr6F19kpa0aYoBWUM11xDPQQfMeS5vw0`;
+    makeRequest("GET",url,function(response){
+        console.log(typeof(response));
+        console.log(response);
+        let data = response.results[0].address_components;
+        for(let i = 0; i < data.length; i++)
+        {
+            if(data[i].types[0] == "administrative_area_level_1"){
+                console.log(data[i].long_name + "X")
+                item.innerText = data[i].long_name;
+                return;
+            }
+        }
+        item.innerText = "";
+        return;
+    })
+}
+
+exports = { controlCoordinate, getCity }
