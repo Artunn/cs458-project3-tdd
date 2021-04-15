8501563780626168e2c5f728071a56fcb72ca5ad
Artun <curaartun@gmail.cm>
2021-04-15 14:29:39 +0300

    existance test for map

--- webpage-source/tests/partoneselenium.js
+++ webpage-source/tests/partoneselenium.js
@@ -1,7 +1,15 @@
-const {Builder, By, Key, util} = require("selenium-webdriver")
+const {Builder, By, Key, util} = require("selenium-webdriver");
+const{assert} = require("assert");
+
+const url = "file:///W:/Desktop/Bilkent/CS458/Projects/cs458-project3-tdd/webpage-source/index.html";
 async function example(){
     let driver = await new Builder().forBrowser("firefox").build();
-    await driver.get("http://google.com");
-    await driver.findElement(By.name("q")).sendKeys("Selenium",Key.RETURN);
+    await driver.get(url);
+    try{
+     await driver.findElement(By.id('parta-map')).then((data) => assert.notEqual(null,data,'element exists!'))
+    }catch(e){
+      console.error("parta-map does not exist!")
+    }
+    driver.close()
 }
 example();
