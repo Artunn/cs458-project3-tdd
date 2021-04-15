e7ac75ae4232f89a8f892b14cd6a62d6592c1b77
yusufavci99 <yusufavci99@gmail.com>
2021-04-16 02:13:14 +0300

    Updated test2

--- webpage-source/tests/parttwo.test.js
+++ webpage-source/tests/parttwo.test.js
@@ -1,7 +1,8 @@
 const parttwo = require('../parttwo');
 
-test('Check Distance Calculating',()=>{
-    // Distance From Hatay To Ankara in KM
-    expect(parttwo.calculateDistance({lat: 36.216667, lng: 36.166667}, {lat: 39.93, lng: 32.85})).toBeCloseTo(504.47, 1);
-    expect(parttwo.calculateDistance({lat: 15.0, lng: -30.0}, {lat: 15.0, lng: 45.0})).toBeCloseTo(8020.42, 1);
+test.each([
+    [{lat: 36.216667, lng: 36.166667}, {lat: 39.93, lng: 32.85}, 504.47],
+    [{lat: 15.0, lng: -30.0}, {lat: 15.0, lng: 45.0}, 8020.42],
+  ])('Check Distance Calculating',(coordinate1, coordinate2, expectedDistance)=>{
+    expect(parttwo.calculateDistance(coordinate1, coordinate2)).toBeCloseTo(expectedDistance, 1);
 })
