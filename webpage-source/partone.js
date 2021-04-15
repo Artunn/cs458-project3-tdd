7ff7a05203c0e9a8a2ae14b97ebed53bcb46b920
Artun <curaartun@gmail.cm>
2021-04-15 15:35:16 +0300

    Added style js and some code to index

--- webpage-source/partone.js
+++ webpage-source/partone.js
@@ -13,7 +13,6 @@ function getCity(coordinateNum1, coordinateNum2) {
     const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinateNum1},${coordinateNum2}&key=AIzaSyDFr6F19kpa0aYoBWUM11xDPQQfMeS5vw0`;
     return axios.get(url).then(function(response){
         let data = response.data.results[0].address_components;
-        // console.log(data);
         for(let i = 0; i < data.length; i++)
         {
             if(data[i].types[0] == "administrative_area_level_1"){
@@ -21,6 +20,15 @@ function getCity(coordinateNum1, coordinateNum2) {
             }
         }
     });
+}
+
+
+let map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
 
 module.exports = { controlCoordinate, getCity }
--- webpage-source/tests/partoneselenium.js
+++ webpage-source/tests/partoneselenium.js
@@ -1,15 +1,16 @@
-const {Builder, By, Key, util} = require("selenium-webdriver");
-const{assert} = require("assert");
+const { Builder, By, Key, util, until } = require("selenium-webdriver");
+const { assert } = require("assert");
 
 const url = "file:///W:/Desktop/Bilkent/CS458/Projects/cs458-project3-tdd/webpage-source/index.html";
-async function example(){
-    let driver = await new Builder().forBrowser("firefox").build();
-    await driver.get(url);
-    try{
-     await driver.findElement(By.id('parta-map')).then((data) => assert.notEqual(null,data,'element exists!'))
-    }catch(e){
-      console.error("parta-map does not exist!")
-    }
-    driver.close()
+async function example() {
+  let driver = await new Builder().forBrowser("firefox").build();
+  await driver.get(url);
+  try{
+    let el = await driver.findElement(By.id(`parta-map`));
+    await driver.wait(until.elementIsVisible(el),100);
+  }catch(e){
+    console.log("No element")
+  }
 }
+
 example();
