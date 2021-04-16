74c8143c2dc379e49fe147716fedbe31a245bd25
yusufavci99 <yusufavci99@gmail.com>
2021-04-16 04:33:57 +0300

    up

--- webpage-source/tests/parttwo.test.js
+++ webpage-source/tests/parttwo.test.js
@@ -3,6 +3,7 @@ const parttwo = require('../parttwo');
 test.each([
     [{lat: 36.216667, lng: 36.166667}, {lat: 39.93, lng: 32.85}, 504.47],
     [{lat: 15.0, lng: -30.0}, {lat: 15.0, lng: 45.0}, 8020.42],
-  ])('Check Distance Calculating',(coordinate1, coordinate2, expectedDistance)=>{
-    expect(parttwo.calculateDistance(coordinate1, coordinate2)).toBeCloseTo(expectedDistance, 1);
+  ])('Check Distance Calculating',async (coordinate1, coordinate2, expectedDistance)=>{
+    let distance = await parttwo.calculateDistance(coordinate1, coordinate2);
+    expect( distance).toBeCloseTo(expectedDistance, 1);
 })
